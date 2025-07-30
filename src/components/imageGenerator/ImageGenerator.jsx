ece034383c46c35be76b1207d1975e8b1852ab33
KarinaKKarinaK <karinakalicka@gmail.com>
2025-07-30 10:28:58 +0200

    Update

--- src/components/imageGenerator/ImageGenerator.jsx
+++ src/components/imageGenerator/ImageGenerator.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./ImageGenerator.css"; 
 
 const ImageGenerator = () => {
-  return <div>ImageGenerator</div>;
+  return (
+    <div className="ai image-generator">
+        <div className="header">AI Image <span></span></div>
+    </div>
+  )
 };
 
 export default ImageGenerator;
