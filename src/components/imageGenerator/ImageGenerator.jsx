abce27ff84221e0d29b7708a59552768c211f5c4
KarinaKKarinaK <karinakalicka@gmail.com>
2025-08-04 23:11:42 +0200

    Updates to the default inage for loading display

--- src/components/imageGenerator/ImageGenerator.jsx
+++ src/components/imageGenerator/ImageGenerator.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./ImageGenerator.css"; 
+import default_image from "../../../assets/default_image.svg"; // Assuming you have a default image in this path
 
 const ImageGenerator = () => {
   return (
     <div className="ai image-generator">
         <div className="header">AI Image <span>generator</span></div>
         <div className="img-loading">
-          <div className="image"></div>
+          <div className="image"><img src={default_image} alt=""></img></div>
         </div>
     </div>
   )
