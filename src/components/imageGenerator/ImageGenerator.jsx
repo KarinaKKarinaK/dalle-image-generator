9d34e9816bc8d093281fbe647dd20d5fbb24f45d
KarinaKKarinaK <karinakalicka@gmail.com>
2025-08-06 13:55:00 +0200

    updating input for image generation

--- src/components/imageGenerator/ImageGenerator.jsx
+++ src/components/imageGenerator/ImageGenerator.jsx
@@ -1,22 +1,34 @@
-import React from "react";
-import "./ImageGenerator.css"; 
+import React, { useRef, useState } from "react";
+import "./ImageGenerator.css";
 import default_image from "../assets/default_image.svg"; // Assuming you have a default image in this path
 
 const ImageGenerator = () => {
+  const [image_url, setImage_url] = useState("/");
+  let inputRef = useRef(null);
+
   return (
     <div className="ai image-generator">
-        <div className="header">AI Image <span>Generator</span></div>
-        <div className="img-loading">
-          <div className="image"><img src={default_image} alt=""></img></div>
-        </div>
-
-        <div className="search-box">
-          <input type="text" className="search-input" placeholder="Describe your image..." />
-          <div className="generate-btn">Generate</div>
-            {/* <button className="generate-btn">Generate</button> */}
+      <div className="header">
+        AI Image <span>Generator</span>
+      </div>
+      <div className="img-loading">
+        <div className="image">
+          <img src={image_url === "/" ? default_image : image_url} alt=""></img>
         </div>
       </div>
-  )
+
+      <div className="search-box">
+        <input
+          type="text"
+          ref={inputRef}
+          className="search-input"
+          placeholder="Describe your image..."
+        />
+        <div className="generate-btn">Generate</div>
+        {/* <button className="generate-btn">Generate</button> */}
+      </div>
+    </div>
+  );
 };
 
 export default ImageGenerator;
