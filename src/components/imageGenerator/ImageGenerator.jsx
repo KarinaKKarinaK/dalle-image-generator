b7d57bffe1934e221a2d1a2841622da602ac49fe
KarinaKKarinaK <karinakalicka@gmail.com>
2025-08-06 13:44:27 +0200

    Updated CSS styling

--- src/components/imageGenerator/ImageGenerator.jsx
+++ src/components/imageGenerator/ImageGenerator.jsx
@@ -5,11 +5,17 @@ import default_image from "../assets/default_image.svg"; // Assuming you have a 
 const ImageGenerator = () => {
   return (
     <div className="ai image-generator">
-        <div className="header">AI Image <span>generator</span></div>
+        <div className="header">AI Image <span>Generator</span></div>
         <div className="img-loading">
           <div className="image"><img src={default_image} alt=""></img></div>
         </div>
-    </div>
+
+        <div className="search-box">
+          <input type="text" className="search-input" placeholder="Describe your image..." />
+          <div className="generate-btn">Generate</div>
+            {/* <button className="generate-btn">Generate</button> */}
+        </div>
+      </div>
   )
 };
 
